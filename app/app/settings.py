9e4bd111aba9d539b2b371b25e5101e614bfd3a3
Carlos Zambrana <carzam87@gmail.com>
2023-02-17 17:47:54 -0600

    Fixed SECRET_KEY on setting.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(-^963l(fgi1v!0c9nn5ld_=c3a2stj%)&jor8csq9zf)#sfjz'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-(-^963l(fgi1v!0c9nn5ld_=c3a2stj%)&jor8csq9zf)#sfjz')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
